1771d9fd278696277735cc013f50654e68e57e16
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-05-13 15:22:45 +0530

    imported tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    'nuxt-quasar-ui'
-  ],
+  modules: ['nuxt-quasar-ui', "@nuxtjs/tailwindcss"],
   quasar: { /* */ }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  prefix: "tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
