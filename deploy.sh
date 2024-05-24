e62c320b7e19570066a50037cbb4496a3f113fa2
ktx-kirtan <124270523+ktx-kirtan@users.noreply.github.com>
2024-05-24 18:55:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 npm run generate
 
 # Move the files to S3 bucket for hosting
-aws s3 sync ./.output/public s3://dashboard-converter/  --exclude=".git/*" --profile=prod
+aws s3 sync ./.output/public s3://dashboard-converter/  --exclude=".git/*" --profile=dev
 
 # invalidate cloudfront cache so that latest files can be served
-aws cloudfront create-invalidation --distribution-id ECA4Q6I4IIBLU --paths="/*" --profile=prod
+aws cloudfront create-invalidation --distribution-id ECA4Q6I4IIBLU --paths="/*" --profile=dev
